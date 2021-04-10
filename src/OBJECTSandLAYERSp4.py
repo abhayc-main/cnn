e087d9c02a2a5f61d5e9b81daa9e9dbfe2afca19
abhayc-glitch <abhichebium@gmail.com>
2021-04-10 15:52:35 -0700

    Layer2 Batch Update

--- src/OBJECTSandLAYERSp4.py
+++ src/OBJECTSandLAYERSp4.py
@@ -11,6 +11,13 @@ weights = [[0.2, 0.8, -0.5, 1.0],
 biases = [2.0, 3.0, 0.5]
 # Biases are a vector Quantity because it's a 1D array.
 
+weights2 = [[0.1, -0.14, 0.5],
+[-0.5, 0.12, -0.33],
+[-0.44, 0.73, -0.13]]
+
+biases2 = [-1, 2, -0.5]
+
+
 # y=f(∑i=1nwixi+b)
 # ^^^ Dot product formula
 # THE DOT PRODUCT IS JUST THE PRODUCT OF 2 VECTORS
@@ -18,7 +25,11 @@ biases = [2.0, 3.0, 0.5]
 #Basically since Weights is a Matric it's gonan do the dot product of each vector.
 # IT RETURNS AN ARRAY OF DOT  PRODUCTS
 output = np.dot(inputs, np.array(weights).T) + biases
+layer2_outputs = np.dot(output, np.array(weights2).T) + biases2
+print(output)
+print("           ")
+print(layer2_outputs)
 # .T is the transposing function and the np.array creates an array
 # WHen transposing we can choose either one to swtich the R and C 
 # WHen we pass the weights and the inputs to numpy, arrays for the weights and the inputs are being created. <BackENd>
-print(output)
+
