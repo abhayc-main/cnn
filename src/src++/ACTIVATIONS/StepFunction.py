782bbc62c990ae051af98ab14a878c6eeb8550df
Abhayc <you@example.com>
2021-05-12 18:01:19 -0700

    Started on Cross Validation

--- src/src++/ACTIVATIONS/StepFunction.py
+++
@@ -1,5 +0,0 @@
-#The simplest activation function is a step function. 
-# In a single neuron, if the weights · inputs + bias results in a value greater than 0, the neuron will fire and output a one, otherwise, it will output a 0.
-# Pretty Trash function NGL
-# Rarelly used adn we are only learning about to get an idea.
-# It's a linear function aka HOT GARBAGE.
