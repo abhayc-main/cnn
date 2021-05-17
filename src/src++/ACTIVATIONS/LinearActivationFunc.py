8f76e2166152d78c58ba1e87960fc281c7124ae9
Abhayc <you@example.com>
2021-05-17 07:51:43 -0700

    Wsl Dev and changing lib configurations

--- src/src++/ACTIVATIONS/LinearActivationFunc.py
+++
@@ -1,8 +0,0 @@
-# This function is exactly like a line.
-# HEllA linear
-# A linear function is simply the equation of a line. It will appear as a straight line when graphed,
-# where y = x and the output value equals the input.
-#This activation function is usually applied to the last layer’s output in the case of a regression model — a model that outputs a scalar value instead of a classification.
-# Y = mx + b
-# y = x
-
